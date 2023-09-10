11b881f06b1584eeb3693a05d2d1d0fa3908dac8
Ikontax <iva.kowalski.de@gmail.com>
2023-09-10 13:07:55 +0200

    timmy the turtle begining

--- Day 18/start.py
+++ Day 18/start.py
@@ -1,11 +1,35 @@
 from turtle import Turtle, Screen
-
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
+import random
 
 
-
-
+tim = Turtle()
 screen = Screen()
+
+
+colors = ["yellow", "dark orange", "medium slate blue", "spring green", "deep pink", "dodger blue"]
+tim.speed("fast")
+tim.width(10)
+
+is_in_screen = True
+
+while is_in_screen:
+    tim.pencolor(random.choice(colors))
+    direction = random.randrange(0, 2)
+    if direction == 0:
+        tim.right(90)
+    else:
+        tim.left(90)
+    tim.forward(random.randint(0, 100))
+        
+
+    
+
+    
+    
+
+
+
+
+
+x
 screen.exitonclick()
